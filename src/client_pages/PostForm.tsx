9584e17c503b06f88183e6b15b692495e3001e45
Nguyen Duc Anh Tuan <tuan.nguyenducanh@gmail.com>
2022-03-30 17:03:19 +0700

    Submit post

--- src/client_pages/PostForm.tsx
+++ src/client_pages/PostForm.tsx
@@ -1,7 +1,19 @@
 import React from 'react';
 import { SubmitHandler, useForm } from 'react-hook-form';
+import {createPost} from '../client_api/post';
+import { useNavigate } from 'react-router-dom';
+
+export type POST_TYPE = {
+    title: string,
+    desc: string,
+    author: string,
+    category: string,
+    thumbnail: string,
+    status: number
+};
 
 export default function PostForm() {
+    const navigate = useNavigate();
     // react hook form
     // Khai báo biến register để đăng ký cho các
     // thành phần trong form
@@ -13,44 +25,83 @@ export default function PostForm() {
     } = useForm({
         defaultValues: {
             title: '',
-            desc: ''
+            desc: '',
+            author: '',
+            category: '',
+            thumbnail: '',
+            status: 0
         }
     });
 
-    console.log(errors);
+    const handleSubmitPost = async (data: POST_TYPE) => {
+        const response = await createPost(data);
 
-    const onSubmit: SubmitHandler<{
-        title: string,
-        desc: string
-    }> = (data) => {
-        console.log(data);
+        if (response.status === 201) {
+            navigate('/posts');
+        }
+    }
+
+    const onSubmit: SubmitHandler<POST_TYPE> = (data) => {
+        handleSubmitPost(data);
     };
-
+    // "author": "tuannda3",
+    // "category": "Tin tuc",
+    // "thumbnail": "",
+    // "status": 1
   return (
     <div>
         <form onSubmit={handleSubmit(onSubmit)}>
-            <input
-                type="text"
-                placeholder='Tiêu đề'
-                {...register(
-                    'title',
-                    {
-                        required: {value: true, message: 'Bat buoc'},
-                    }
-                )}
-            />
-            <div>{errors.title  ? 'Bắt buộc' : ''}</div>
-            <div>{errors.title?.message || 'Bắt buộc'}</div>
-            <input
-                type="text"
-                placeholder='Mô tả'
-                {...register(
-                    'desc',
-                    {
-                        required: true
-                    }
-                )}
-            />
+            <div>
+                <label htmlFor="">Title</label>
+                <input
+                    type="text"
+                    placeholder='Tiêu đề'
+                    {...register(
+                        'title',
+                        {
+                            required: {value: true, message: 'Bắt buộc'},
+                        }
+                    )}
+                />
+                <div>{errors.title?.message}</div>
+            </div>
+            <div>
+                <label htmlFor="">Description</label>
+                <input
+                    type="text"
+                    placeholder='Mô tả'
+                    {...register(
+                        'desc',
+                        {
+                            required: true
+                        }
+                    )}
+                />
+            </div>
+            <div>
+                <label htmlFor="">Author</label>
+                <input type="text" placeholder='author'
+                    {...register('author')}
+                />
+            </div>
+            <div>
+                <label htmlFor="">Category</label>
+                <input type="text" placeholder='category'
+                    {...register('category')}
+                />
+            </div>
+            <div>
+                <label htmlFor="">Thumbnail</label>
+                <input type="text" placeholder='thumbnail'
+                    {...register('thumbnail')}
+                />
+            </div>
+            <div>
+                <label htmlFor="">Status</label>
+                <input type="radio" {...register('status')} value={0} />Không kích hoạt
+                <input type="radio" {...register('status')} value={1} />Kích hoạt
+            </div>
+
             <button>Submit</button>
         </form>
     </div>
